1b6ab1bed8b627ea503a069a824341427f224afa
marcos-px <marcxstx@gmail.com>
2022-09-10 19:40:23 -0300

    criação de models

---
+++ src/models/Pacientes.js
@@ -0,0 +1,36 @@
+const db = require("../database");
+const { DataTypes } = require("sequelize");
+
+const Psicologos = db.define(
+    "Psicologos",
+    {
+        psicologo_id: {
+            type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        },
+        nome:{
+            type: DataTypes.STRING,
+        },
+        email:{
+            type: DataTypes.STRING,
+        },
+        senha:{
+            type: DataTypes.STRING,
+        },
+        apresentacao:{
+            type: DataTypes.STRING,
+        },
+        createdAt:{
+            type: DataTypes.DATE,
+        },
+        updatedAt:{
+            type: DataTypes.DATE,
+        },
+    },
+    {
+        tableName: "psicologo",
+    }
+);
+
+module.exports = Psicologos;
