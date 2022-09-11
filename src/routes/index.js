225a5ff69a64a79691e622bf0add16ef8d391ae8
paula-beauclair <paulabeauclair95@gmail.com>
2022-09-11 13:40:28 -0300

    busca por id

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,4 +6,10 @@ const pacientesController = require("../controllers/pacientesController")
 const routes = express.Router();
 
 routes.get("/pacientes", pacientesController.listarPacientes);
+routes.get("/pacientes/:id", pacientesController.listaPacienteID);
+routes.post("/pacientes", pacientesController.cadastraPaciente)
+
+
+
+
 module.exports = routes;
