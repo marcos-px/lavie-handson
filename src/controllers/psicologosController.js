83bc51e0997d188d4d7f9e52c262155e0e74f322
Lean Douglas <87292177+LeanDHC@users.noreply.github.com>
2022-09-12 12:34:42 -0300

    Inclusão do metodo Delete

--- src/controllers/psicologosController.js
+++ src/controllers/psicologosController.js
@@ -22,12 +22,12 @@ const psicologosController = {
 
     async cadastrarPsicologo(req,res){
         try{
-            const {nome,email,senha,apresentação} = req.body;
+            const {nome,email,senha,apresentacao} = req.body;
             const novoPsicologo = await Psicologos.create({
                 nome,
                 email,
                 senha,
-                apresentação
+                apresentacao,
             });
             if(novoPsicologo.length== 0){
                 return res.status(400).send("Dados não informados.")
@@ -38,7 +38,35 @@ const psicologosController = {
             console.error(error);
             res.status(400).send(" Ocorreu um erro ao cadastrar o psicologo, contate o suporte.");
         }
+    },
+
+    async deletarPsicologo(req,res){
+        try {
+            const { id } = req.params;
+
+    const destroyPsicologo = await Psicologos.findByPk(id)
+
+    if (!destroyPsicologo){
+        return res.status(404).send("Id não encontrado!");
+    };
+
+    const deletaPsicologo = await Psicologos.destroy({
+        where:{
+            psicologo_id: id
+        }
+    });
+    
+    return res.status(204).json(`Psicologo ${ deletaPsicologo} deletado com sucesso!`);
+    
+
+        } catch (error) {
+    console.error(error);
+    return res.status(404).json("Ocorreu um erro na requisição ao deletar, contate o suporte!")
+        }
     }
+
+   
+    
 
 };
 
